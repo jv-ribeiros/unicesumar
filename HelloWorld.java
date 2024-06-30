4218cc5a94a6d01f2bece0d2e87bd9adb83eaa0d
João Ribeiro <joaovictor-rb@hotmail.com>
2024-06-30 17:29:54 -0300

    Primeiro commit: Adicionando projeto HelloWorld

---
+++ HelloWorld.java
@@ -0,0 +1,11 @@
+
+/**
+ * RA: 21049443-5
+ * @author João Victor Ribeiro
+ */
+
+public class HelloWorld {
+  public static void main(String[] args) {
+    System.out.println("Hello, World!");
+  }
+}
